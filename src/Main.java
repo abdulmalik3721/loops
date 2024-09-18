6a3f442aa1a30dde46191bce809becfa55d5d989
ABDUL MALIK <twb@gmail.com>
2024-09-18 08:51:21 +0530

    example of for loop

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,24 @@ public class Main {
         double result =calculateInterest(10.2,21.4);
         System.out.println("the result of interest is: " + result);
         System.out.println("the result is: " + calculateInterest(12.4,87.8));
+
         for(double rate=2.0;rate<=5.0;rate++){
             double interestAmount = calculateInterest(10000,rate);
             System.out.println("10,100 at " + rate + " % interest = " + interestAmount);
+        }
+
+        for(double i =7.5;i<=10;i+=0.25){
+            double interestAmout = calculateInterest(100.00,i);
+            if(interestAmout>8.5){
+                break;
+            }
+            System.out.println("$100.00 " + i + " % interest = $" + interestAmout);
         }
     }
 
     public static double calculateInterest(double amount, double interestRate){
         return (amount*(interestRate/100));
     }
+
+
 }
